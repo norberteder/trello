9e277a6402470534976eb63b56a0d4ed728a0c0b
Norbert Eder <wpfnerd+github@gmail.com>
2013-04-26 13:39:16 +0200

    removed restler due to inactivity and incompatibility to node 0.10.x; added 'getCardsForList' and 'getListsOnBoardByFilter'

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# Trello
+# trello_ex
 ## A simple asynchronous client for [Trello](http://www.trello.com)
 
 This is a wrapper for some of the Trello HTTP API. Please feel free to add any other pieces you need! :)
 
+  The original version was published [here](https://github.com/GraemeF/trello), but isn't maintained any more. I added some new functions for a better support of Trello; also node module 'restler' has been replaced due to compatibility issues with node 0.10.x (and it seems not to be maintained anymore).
+
 ## Installation
-    npm install trello
+    npm install trello_ex
 
 ## Usage
 First, generate your Trello application key [like this](https://trello.com/card/generating-your-developer-key/4ed7e27fe6abb2517a21383d/25), then follow [these confusing instructions](https://trello.com/card/getting-a-user-token-and-oauth-urls/4ed7e27fe6abb2517a21383d/26) to get a user token. These need to be supplied when you create the Trello object (see below).
  
 ## Example
 ```javascript
-  var Trello = require("trello");
+  var Trello = require("trello_ex");
   var trello = new Trello("MY APPLICATION KEY", "MY USER TOKEN");
 
   trello.addCard('Clean car', 'Wax on, wax off', myListId,
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var rest = require('restler');
+var rest = require('rest-client');
 
 var Trello = function (key, token) {
     this.uri = "https://api.trello.com";
@@ -10,15 +10,18 @@ Trello.prototype.createQuery = function () {
     return {key: this.key, token: this.token};
 };
 
-function makeRequest(fn, uri, options, callback) {
-    fn(uri, options)
-        .on('complete', function (result) {
-            if (result instanceof Error) {
-                callback(result);
-            } else {
-                callback(null, result);
-            }
-        });
+function makeRequest(method, uri, options, callback) {
+    rest.send({
+        url: uri,
+        method: method,
+        form: options
+    }, function(res, body) {
+        if (res.statusCode === 200) {
+            callback(null, body);
+        } else {
+            callback(new Error(body));
+        }
+    });
 }
 
 Trello.prototype.addBoard = function (name, description, organizationId, callback) {
@@ -30,7 +33,7 @@ Trello.prototype.addBoard = function (name, description, organizationId, callbac
     if (organizationId !== null)
         query.idOrganization = organizationId;
 
-    makeRequest(rest.post, this.uri + '/1/boards', {query: query}, callback);
+    makeRequest('POST', this.uri + '/1/boards', query, callback);
 };
 
 Trello.prototype.addCard = function (name, description, listId, callback) {
@@ -41,48 +44,61 @@ Trello.prototype.addCard = function (name, description, listId, callback) {
     if (description !== null)
         query.desc = description;
 
-    makeRequest(rest.post, this.uri + '/1/cards', {query: query}, callback);
+    makeRequest('POST', this.uri + '/1/cards', query, callback);
 };
 
 Trello.prototype.getCard = function (boardId, cardId, callback) {
-    makeRequest(rest.get, this.uri + '/1/boards/' + boardId + '/cards/' + cardId, {query: this.createQuery()}, callback);
+    makeRequest('GET', this.uri + '/1/boards/' + boardId + '/cards/' + cardId, this.createQuery(), callback);
+};
+
+Trello.prototype.getCardsForList = function(listId, actions, callback) {
+    var query = this.createQuery();
+    if (actions)
+        query.actions = actions;
+    makeRequest('GET', this.uri + '/1/lists/' + listId + '/cards', query, callback);
 };
 
 Trello.prototype.addListToBoard = function (boardId, name, callback) {
     var query = this.createQuery();
     query.name = name;
 
-    makeRequest(rest.post, this.uri + '/1/boards/' + boardId + '/lists', {query: query}, callback);
+    makeRequest('POST', this.uri + '/1/boards/' + boardId + '/lists', query, callback);
 };
 
 Trello.prototype.addCommentToCard = function (cardId, comment, callback) {
     var query = this.createQuery();
     query.text = comment;
 
-    makeRequest(rest.post, this.uri + '/1/cards/' + cardId + '/actions/comments', {query: query}, callback);
+    makeRequest('POST', this.uri + '/1/cards/' + cardId + '/actions/comments', query, callback);
 };
 
 Trello.prototype.addMemberToCard = function (cardId, memberId, callback) {
     var query = this.createQuery();
     query.value = memberId;
 
-    makeRequest(rest.post, this.uri + '/1/cards/' + cardId + '/members', {query: query}, callback);
+    makeRequest('POST', this.uri + '/1/cards/' + cardId + '/members', query, callback);
 };
 
 Trello.prototype.getBoardMembers = function (boardId, callback) {
-    makeRequest(rest.get, this.uri + '/1/boards/' + boardId + '/members', {query: this.createQuery()}, callback);
+    makeRequest('GET', this.uri + '/1/boards/' + boardId + '/members', this.createQuery(), callback);
 };
 
 Trello.prototype.getListsOnBoard = function (boardId, callback) {
-    makeRequest(rest.get, this.uri + '/1/boards/' + boardId + '/lists', {query: this.createQuery()}, callback);
+    makeRequest('GET', this.uri + '/1/boards/' + boardId + '/lists', this.createQuery(), callback);
+};
+
+Trello.prototype.getListsOnBoardByFilter = function(boardId, filter, callback) {
+    var query = this.createQuery();
+    query.filter = filter;
+    makeRequest('GET', this.uri + '/1/boards/' + boardId + '/lists', query, callback);
 };
 
 Trello.prototype.getCardsOnBoard = function (boardId, callback) {
-    makeRequest(rest.get, this.uri + '/1/boards/' + boardId + '/cards', {query: this.createQuery()}, callback);
+    makeRequest('GET', this.uri + '/1/boards/' + boardId + '/cards', this.createQuery(), callback);
 };
 
 Trello.prototype.deleteCard = function (cardId, callback) {
-    makeRequest(rest.del, this.uri + '/1/cards/' + cardId, {query: this.createQuery()}, callback);
+    makeRequest('DELETE', this.uri + '/1/cards/' + cardId, this.createQuery(), callback);
 };
 
 module.exports = Trello;
